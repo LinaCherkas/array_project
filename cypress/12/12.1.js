b24f8520d7a8f4bd91a2e14be942de4254c7902b
Anhelina <anhelina@wsd-149-71b.local>
2022-05-05 01:18:38 +0300

    Initial commit

---
+++ cypress/12/12.1.js
@@ -0,0 +1,63 @@
+import 'chance'
+import {isSuperset, difference, union, intersection} from "../utils/helper"
+
+it('Array task 1-5', () => {
+
+//task 1
+    let currencySet = new Set(['RUB', 'BYN', 'EUR'])
+
+//task 2
+    cy.log("First array output")
+    currencySet.forEach(currency => {
+        cy.log(currency)
+    });
+
+//task 3
+    currencySet.add('PLN').add('PLN').add('USD')
+
+    let additional_currency = new Set(['UAN', 'UAN', 'SEK'])
+    additional_currency.forEach(currency => {
+        currencySet.add(currency)
+    })
+
+    cy.log("Second array output")
+    currencySet.forEach(currency => {
+        cy.log(currency)
+    });
+
+//task 4
+    cy.log("Set has UAN value: " + currencySet.has('UAN'))
+    currencySet.delete('UAN')
+    cy.log("Set has UAN value: " + currencySet.has('UAN'))
+
+//task 5
+    let currencyArray = Array.from(currencySet)
+    cy.log("1 random value: " + chance.pickone(currencyArray))
+    cy.log("Few random values: " + chance.pickset(currencyArray, chance.integer({min:1, max:currencySet.size})))
+
+})
+
+//task 6
+it('Array task 6', () => {
+    let setA = new Set(['RUB', 'BYN', 'EUR', 'UAN'])
+    let setB = new Set(['USD', 'RUB', 'UAN'])
+    let setC = new Set(['RUB', 'SEK', 'BYN', 'PLN'])
+
+    cy.log("isSuperset:" + isSuperset(setA, setB))
+
+    cy.log("Union:")
+    union(setA, setC).forEach(currency => {
+        cy.log(currency)
+    })
+
+    cy.log("Intersection:")
+    intersection(setA, setB).forEach(currency => {
+        cy.log(currency)
+    })
+
+    cy.log("Difference:")
+    difference(setA, setC).forEach(currency => {
+        cy.log(currency)
+    })
+})
+
---
+++ cypress/utils/helper.js
@@ -0,0 +1,35 @@
+
+export const isSuperset = (set, subset) =>  {
+    subset.forEach( elem => {
+        if(!set.has(elem)) {
+            return false
+        }
+    })
+    return true
+}
+
+export const union = (setA, setB) => {
+    let _union = new Set(setA)
+    setB.forEach( elem => {
+        _union.add(elem)
+    })
+    return _union
+}
+
+export const intersection = (setA, setB) => {
+    let _intersection = new Set()
+    setB.forEach( elem => {
+        if (setA.has(elem)) {
+            _intersection.add(elem)
+        }
+    })
+    return _intersection
+}
+
+export const difference = (setA, setB) => {
+    let _difference = new Set(setA)
+    setB.forEach( elem => {
+        _difference.delete(elem)
+    })
+    return _difference
+}
