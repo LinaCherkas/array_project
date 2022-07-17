040c23b58d70a61c74becd83a8363276e5b4e528
merrybeth <legerumia@gmail.com>
2022-07-17 22:29:09 +0300

    ifElseTask fixes

--- cypress/13/ifElseTask.js
+++ cypress/13/ifElseTask.js
@@ -1,36 +1,41 @@
 import Chance from 'chance'
 const chance = new Chance()
 
-let randomAge = chance.age()
-
-//method with if
-if (randomAge < 13) console.log(`${randomAge} is age for child`)
-else if (randomAge >= 13 && randomAge < 20)
-    console.log(`${randomAge} is age for teen`)
-else if (randomAge >= 20 && randomAge < 60)
-    console.log(`${randomAge} is age for adult`)
-else console.log(`${randomAge} is age for senior`)
-
-//method with switch
-switch (true) {
-    case randomAge < 13:
-        console.log(`${randomAge} is age for child`)
-        break
-    case randomAge >= 13 && randomAge < 20:
-        console.log(`${randomAge} is age for teen`)
-        break
-    case randomAge >= 20 && randomAge < 60:
-        console.log(`${randomAge} is age for adult`)
-        break
-    default:
-        console.log(`${randomAge} is age for senior`)
+function if_method(randomAge) {
+    if (randomAge < 13) console.log(`${randomAge} is the age for child`)
+    else if (randomAge >= 13 && randomAge < 20)
+        console.log(`${randomAge} is the age for teen`)
+    else if (randomAge >= 20 && randomAge < 60)
+        console.log(`${randomAge} is the age for adult`)
+    else console.log(`${randomAge} is the age for senior`)
 }
 
-//method with operator
+function switch_method(randomAge){
+switch (true) {
+    case randomAge < 13:
+        console.log(`${randomAge} is the age for child`)
+        break
+    case randomAge >= 13 && randomAge < 20:
+        console.log(`${randomAge} is the age for teen`)
+        break
+    case randomAge >= 20 && randomAge < 60:
+        console.log(`${randomAge} is the age for adult`)
+        break
+    default:
+        console.log(`${randomAge} is the age for senior`)
+}}
+
+function operator_method(randomAge){
 randomAge < 13
-    ? console.log(`${randomAge} is age for child`)
+    ? console.log(`${randomAge} is the age for child`)
     : randomAge >= 13 && randomAge < 20
-    ? console.log(`${randomAge} is age for teen`)
+    ? console.log(`${randomAge} is the age for teen`)
     : randomAge >= 20 && randomAge < 60
-    ? console.log(`${randomAge} is age for adult`)
-    : console.log(`${randomAge} is age for senior`)
+    ? console.log(`${randomAge} is the age for adult`)
+    : console.log(`${randomAge} is the age for senior`)
+}
+
+let randomAge = chance.age()
+if_method(randomAge)
+switch_method(randomAge)
+operator_method(randomAge)
