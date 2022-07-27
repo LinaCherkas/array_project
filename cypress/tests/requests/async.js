c47c8d9d37579eeed09d8d2e838946dd0ce67971
merrybeth <legerumia@gmail.com>
2022-07-27 23:18:31 +0300

    Async task fixes

--- cypress/tests/requests/async.js
+++ cypress/tests/requests/async.js
@@ -1,64 +1,53 @@
 const requestProducts =
     'https://storage.googleapis.com/mannequin/2018/data/productwall/accessories/en_us.json?c=1571310916'
 
-function printFirstObject(object) {
-    cy.log('First object data:')
-    cy.log(
-        Object.keys(object)
-            .map((key) => key + ':' + object[key])
-            .join(', ')
-    )
-}
+describe('Async tests', function () {
+    describe('Test 1', function () {
+        before(() => {
+            cy.request('GET', requestProducts).its('body').as('response')
+        })
 
-function printObjectLength(object) {
-    cy.log('Object length:')
-    let k = 0
-    for (let j = 0; j < object.filter_groups[0].subfilters.length; j++) {
-        k += object.filter_groups[0].subfilters[j].filters.length
-    }
-    cy.log(k)
-}
-
-before(() => {
-    cy.request(requestProducts).its('body').as('products')
-})
-
-it('Test 1 with before', function () {
-    debugger
-    let firstObject = this.products.filter_groups[0].subfilters[0].filters[0]
-    printFirstObject(firstObject)
-    printObjectLength(this.products)
-})
-
-it('Test 2 with it', function () {
-    cy.request(requestProducts).then((response) => {
-        cy.wrap(response.body).as('products')
+        it('Test 1 with before', function () {
+            printObject(this.response.products[0])
+            cy.log(`Products length - ${this.response.products.length}`)
+        })
     })
 
-    cy.get('@products').then((productsList) => {
-        let firstObject = productsList.filter_groups[0].subfilters[0].filters[0]
-        printFirstObject(firstObject)
-    })
-
-    cy.get('@products').then((productsList) => {
-        printObjectLength(productsList)
-    })
-})
-
-it('Test 3 with promise', function () {
-    let getPromise = new Promise((resolve, reject) => {
-        try {
-            cy.request(requestProducts).then((response) => {
-                resolve(response.body)
+    describe('Test 2', function () {
+        it('Test 2 with it', function () {
+            cy.request('GET', requestProducts).then((response) => {
+                cy.wrap(response.body).as('response')
             })
-        } catch (err) {
-            reject(`Error in promise ${err}`)
-        }
+
+            cy.get('@response').then((productsList) => {
+                printObject(productsList.products[0])
+                cy.log(`Products length - ${productsList.products.length}`)
+            })
+        })
     })
 
-    getPromise.then((data) => {
-        let firstObject = data.filter_groups[0].subfilters[0].filters[0]
-        printFirstObject(firstObject)
-        printObjectLength(data)
+    describe('Test 3', function () {
+        it('Test 3 with promise', function () {
+            getPromise().then((data) => {
+                printObject(data.products[0])
+                cy.log(`Products length - ${data.products.length}`)
+            })
+        })
     })
 })
+
+function printObject(object) {
+    cy.log(`Object data:
+        ${Object.keys(object)
+            .map((key) => key + ':' + object[key])
+            .join(', ')}`)
+}
+
+function getPromise() {
+    return new Promise((resolve, reject) => {
+        cy.request('GET', requestProducts).then((response) => {
+            resolve(response.body)
+            reject(`Error in promise`)
+        })
+    })
+}
